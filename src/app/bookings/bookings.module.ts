6fbee2a20418a1a958efd86ad5e70cde4bb9619f
Behram <ikhanbehram@gmail.com>
2021-08-01 17:54:40 +0500

    layout finished

--- src/app/bookings/bookings.module.ts
+++ src/app/bookings/bookings.module.ts
@@ -7,10 +7,9 @@ import { IonicModule } from '@ionic/angular';
 import { BookingsPageRoutingModule } from './bookings-routing.module';
 
 import { BookingsPage } from './bookings.page';
-import { CreateBookingComponent } from './create-booking/create-booking.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, BookingsPageRoutingModule],
-  declarations: [BookingsPage, CreateBookingComponent],
+  declarations: [BookingsPage],
 })
 export class BookingsPageModule {}
